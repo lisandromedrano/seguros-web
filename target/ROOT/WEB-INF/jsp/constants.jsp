f64de8531c4e7efdef1377f7e4e0bafed76bb2ce
lisandro <lisandro.medrano@gmail.com>
2014-06-06 10:05:06 -0300

    removed target

--- target/ROOT/WEB-INF/jsp/constants.jsp
+++
@@ -1,5 +0,0 @@
-<script>
-appcontext= '${context.webapp.name}';
-context="${pageContext.request.contextPath}";
-CONTEXT_ROOT = '<%= request.getContextPath() %>';
-</script>
--- target/ROOT/resources/app/model/Companias.js
+++
@@ -1,9 +0,0 @@
-Ext.define('app.model.Companias', {
-    extend: 'Ext.data.Model',
-    fields: [
-		{name:'id',type:'string'}, 
-		{name:'nombre'}
-    ],
-	idProperty: 'id'
-});
-
